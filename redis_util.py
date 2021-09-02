42517661a1b63606b79355514b20e1e79088acd9
vinay2142070 <vinay2142070@gmail.com>
2021-09-02 20:03:44 +0530

    configured rediscloud_url for heroku redis

--- redis_util.py
+++ redis_util.py
@@ -1,4 +1,7 @@
 import redis
 import os
 
-jwt_redis_blocklist =redis.from_url(os.environ['REDISCLOUD_URL'])
+'''jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)'''
+jwt_redis_blocklist=os.getenv("REDIS_URL") 
