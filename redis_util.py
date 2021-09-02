79495c0e0f58b658aabc32728bdc88610fab82ce
vinay2142070 <vinay2142070@gmail.com>
2021-09-02 22:52:41 +0530

    urlparser dependency added

--- redis_util.py
+++ redis_util.py
@@ -1,9 +1,9 @@
 import redis
 import os
-import urlparser
+import urllib.parse
 
 '''jwt_redis_blocklist = redis.StrictRedis(
     host="localhost", port=6379, db=0, decode_responses=True
 )'''
-url = urlparser.urlparse(os.environ.get('REDIS_URL'))
+url = urllib.parse.urlparse(os.environ.get('REDIS_URL'))
 jwt_redis_blocklist = redis.Redis(host=url.hostname, port=url.port, password=url.password)
